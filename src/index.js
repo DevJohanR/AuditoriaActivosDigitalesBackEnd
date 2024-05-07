298809bb4c6042e343aa944fcb9cd95e41d0f383
DevJohanR <desarrollador7777@gmail.com>
2024-05-07 11:39:28 -0500

    actualizacion ruta absoluta  a ruta relativa usando el modulo path

--- src/index.js
+++ src/index.js
@@ -1,56 +1,30 @@
 const express = require("express");
-
 const cors = require('cors');
-var XLSX =  require ("xlsx")
+const XLSX = require("xlsx");
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const bcrypt = require('bcryptjs');
+const path = require('path'); // Importa el módulo path
 
-const app = express ();
+const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-
-
-//data excel to json
-app.get('/datos', (req,res)=>{
-
-    
-        const excel = XLSX.readFile("C:\\Users\\ejoha\\OneDrive\\Escritorio\\BackendCryptos\\datos.xlsx");
-            
-        var nombreHoja = excel.SheetNames;
-        
-        let datos = XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[0]]);
-        
-        const jDatos = [];
-        for(let i =0; i < datos.length; i++){
-            const dato = datos[i];
-            jDatos.push({
-                ...dato,
-                 FECHA: new Date((dato.FECHA - (25567 + 2)) * 86400 * 1000)
-            });
-        }
-        
-       res.json(jDatos)
-        
-    
+// Data excel to json
+app.get('/datos', (req, res) => {
+    // Construye la ruta del archivo usando path.join para asegurarte de que funcione en cualquier sistema operativo
+    const filePath = path.join(__dirname, '..', 'datos.xlsx');
+    const excel = XLSX.readFile(filePath);
+    var nombreHoja = excel.SheetNames;
+    let datos = XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[0]]);
+    const jDatos = datos.map(dato => ({
+        ...dato,
+        FECHA: new Date((dato.FECHA - (25567 + 2)) * 86400 * 1000)
+    }));
+    res.json(jDatos);
 });
 
-
-//middleware para parsear el cuerpo de las solicitudes http
-app.use (bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
-
-
-
-
-
-
 const PORT = 3001;
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
-
-})
-
-
-
+});
